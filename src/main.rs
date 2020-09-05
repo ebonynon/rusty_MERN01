1f09668ad7aaf37d1bdda55c6d6689c6a37aaaa0
ebonynon <sithumsandeepap@gmail.com>
2020-09-05 12:36:29 +0530

    Cargo watch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use actix_web::get;
 use actix_web::{App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("<h1>Hello world!</h1>")
 }
 #[get("/again")]
 async fn index2() -> impl Responder {
@@ -16,8 +17,14 @@ async fn index3() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(index2).service(index3))
-        .bind("127.0.0.1:8088")?
-        .run()
-        .await
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().service(index).service(index2).service(index3));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8088")?
+    };
+
+    server.run().await
 }
