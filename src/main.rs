d2ab61b84463e5ad6cdfe752d5640a8d020e913b
ebonynon <sithumsandeepap@gmail.com>
2020-09-05 15:37:29 +0530

    list of dbz

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate dotenv;
+extern crate mongodb;
 
-use dotenv::dotenv;
 use actix_web::get;
-use actix_web::{App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+//use dotenv::dotenv;
 use listenfd::ListenFd;
 use mongodb::{options::ClientOptions, Client};
 use std::env;
+
+mod logs_handlers;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -20,15 +23,46 @@ async fn index3() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+// #[cfg(feature = "tokio-runtime")]
+// #[tokio::mongocall]
+// async fn mongocall() -> mongodb::error::Result<()> {
+//     dotenv::dotenv().expect("Failed to read .env file");
+//     let case_sensitive = env::var("MONGODB_URI").expect("MONGODB_URI not found");
+
+//     let mut client_options = ClientOptions::parse(&case_sensitive.to_string()).await?;
+// }
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let mut listenfd = ListenFd::from_env();
-
+    // mongocall()
     dotenv::dotenv().expect("Failed to read .env file");
     let case_sensitive = env::var("MONGODB_URI").expect("MONGODB_URI not found");
-    println!("Env : {}", case_sensitive);
-    
-    let mut server = HttpServer::new(|| App::new().service(index).service(index2).service(index3));
+
+    let mut client_options = ClientOptions::parse(&case_sensitive.to_string())
+        .await
+        .unwrap();
+
+    // let mut client_options = ClientOptions::parse(&case_sensitive.to_string()).await?;
+
+    // Manually set an option.
+    client_options.app_name = Some("XeonAPI".to_string());
+
+    // Get a handle to the deployment.
+    let client = Client::with_options(client_options).unwrap();
+
+    // List the names of the databases in that deployment.
+    for db_name in client.list_database_names(None, None).await.unwrap() {
+        println!("{}", db_name);
+    }
+
+    let mut server = HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(index2)
+            .service(index3)
+            .service(web::scope("/api").configure(logs_handlers::scoped_config))
+    });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
