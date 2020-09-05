415f3b2476efd50f28eda151b7d26d7cc7629020
ebonynon <sithumsandeepap@gmail.com>
2020-09-05 12:24:26 +0530

    #[get("%")]

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use actix_web::get;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder};
 
+#[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
+#[get("/again")]
 async fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello world again!")
 }
@@ -14,13 +16,8 @@ async fn index3() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            //.route("/", web::get().to(index))
-            //.route("/again", web::get().to(index2))
-            .service(index3)
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index).service(index2).service(index3))
+        .bind("127.0.0.1:8088")?
+        .run()
+        .await
 }
