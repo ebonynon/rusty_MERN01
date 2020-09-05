f88a3d4747c4aedc9ad1724ee9294af421b3a696
ebonynon <sithumsandeepap@gmail.com>
2020-09-05 18:40:49 +0530

    dunno

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,43 @@ use mongodb::{
     Client,
 };
 use std::env;
+use std::sync::Mutex;
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("<h1>Hello world!</h1>")
+    HttpResponse::Ok().body("<h2>Welcome to Rust web server!</h2>")
 }
-#[get("/again")]
-async fn index2() -> impl Responder {
+#[get("/books")]
+async fn index_book() -> impl Responder {
+    ///let books_collection = collection();
+
+    //Query the database for all pets which are cats.
+    ////let mut cursor = books_collection.find(doc! {}, None).await.unwrap();
+
+   //// while let Some(doc) = cursor.next().await {
+   ///     println!("{}", doc.unwrap())
+   //// }
+
     HttpResponse::Ok().body("Hello world again!")
 }
-#[get("/hello")]
-async fn index3() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    let mut listenfd = ListenFd::from_env();
+// async fn index_book2() mongodb::Collection {
+//     let books_collection = collection();
 
+//     //Query the database for all pets which are cats.
+//     let mut cursor = books_collection.find(doc! {}, None).await.unwrap();
+
+//     while let Some(doc) = cursor.next().await {
+//         println!("{}", doc.unwrap())
+//     }
+
+// }
+
+// async fn index2(data: mongodb::Collection) -> impl Responder {
+//     HttpResponse::Ok().body("Hello world again!")
+// }
+
+async fn collection() -> mongodb::Collection {
     dotenv::dotenv().expect("Failed to read .env file");
     let case_sensitive = env::var("MONGODB_URI").expect("MONGODB_URI not found");
     // Parse a connection string into an options struct.
@@ -42,40 +61,58 @@ async fn main() -> std::io::Result<()> {
     // Get a handle to the deployment.
     let client = Client::with_options(client_options).unwrap();
 
-    // List the names of the databases in that deployment.
-    for db_name in client.list_database_names(None, None).await.unwrap() {
-        println!("{}", db_name);
-    }
-
-    //let db = client.database("T");
-
     //let books_collection = db.collection("books");
     let books_collection = client.database("T").collection("books");
 
-    // Query the database for all pets which are cats.
+    //Query the database for all pets which are cats.
     let mut cursor = books_collection.find(doc! {}, None).await.unwrap();
-
-    //let mut results = Vec::new();
-    // while let Some(result) = cursor.next().await {
-    //     match result {
-    //         Ok(document) => {
-    //             //results.push(document);
-    //             println!("{}", document);
-    //         }
-    //     }
-    // }
 
     while let Some(doc) = cursor.next().await {
         println!("{}", doc.unwrap())
     }
+}
 
-    let mut server = HttpServer::new(|| {
-        App::new()
-            //.data(cursor.clone())
-            .service(index)
-            .service(index2)
-            .service(index3)
-    });
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+
+    // dotenv::dotenv().expect("Failed to read .env file");
+    // let case_sensitive = env::var("MONGODB_URI").expect("MONGODB_URI not found");
+    // // Parse a connection string into an options struct.
+    // let mut client_options = ClientOptions::parse(&case_sensitive.to_string())
+    //     .await
+    //     .unwrap();
+
+    // // Manually set an option.
+    // client_options.app_name = Some("XeonAPI".to_string());
+
+    // // Get a handle to the deployment.
+    // let client = Client::with_options(client_options).unwrap();
+
+    // // List the names of the databases in that deployment.
+    // for db_name in client.list_database_names(None, None).await.unwrap() {
+    //     println!("{}", db_name);
+    // }
+
+    // //let books_collection = db.collection("books");
+    // let books_collection = client.database("T").collection("books");
+
+    //indexBook(books_collection);
+    //#let fooo = index2(books_collection);
+
+    // Query the database for all pets which are cats.
+    //#let mut cursor = books_collection.find(doc! {}, None).await.unwrap();
+
+    //# while let Some(doc) = cursor.next().await {
+    //#     println!("{}", doc.unwrap())
+    //# }
+
+    let mut server = HttpServer::new(|| App::new().service(index).service(index_book));
+
+    // let mut server = HttpServer::new(|| {
+    //     App::new()
+    //     .route("/again", web::get().to(index2))
+    // });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
